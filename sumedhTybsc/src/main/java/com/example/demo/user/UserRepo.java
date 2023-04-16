3f0c432a5bd30f4cb5a71c43e7e9adcce462a8c5
Sumedh ahire <94281812+darkone03@users.noreply.github.com>
2023-04-16 19:15:31 +0530

    Revert "added user pass table in database and sign in based on that"

--- sumedhTybsc/src/main/java/com/example/demo/user/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepo extends CrudRepository<User, String>{
-	User findByUsername(String username);
-}
